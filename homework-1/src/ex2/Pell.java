c32503d85fd5e1876b2fd79f6d72e8ce56fe51b6
Pavel <pa11ady@yandex.ru>
2023-03-04 02:10:08 +0300

    ex2/Pell

---
+++ homework-1/src/ex2/Pell.java
@@ -0,0 +1,23 @@
+package ex2;
+
+import java.util.Scanner;
+
+public class Pell {
+    static long getPell(int n) {
+        if (0 <= n && n <= 1) return n;
+        long num1 = 0;
+        long num2 = 1;
+        for (int i = 2; i <= n; i++) {
+            long tmp = num2;
+            num2 = 2 * num2 + num1;
+            num1 = tmp;
+        }
+        return num2;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        System.out.println(getPell(n));
+    }
+}
